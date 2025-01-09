3e7115917968cbc85c2913243384ad4880a19e60
Daksh <118788209+DAKSH-K1232@users.noreply.github.com>
2025-01-10 02:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (98.98%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        return ranges::count_if(
+            words,[&](const string& word){return word.find(pref)==0;}
+        );
+    }
+};

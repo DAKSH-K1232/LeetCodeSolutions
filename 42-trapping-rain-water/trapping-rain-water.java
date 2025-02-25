fa942216ca39a5958e7c4fb1d5f1c6e255e6dcb0
Daksh <118788209+DAKSH-K1232@users.noreply.github.com>
2025-02-25 15:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (37.94%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -6,26 +6,26 @@ public class Solution {
         
         int left = 0, right = height.length - 1;  
         int leftMax = 0, rightMax = 0;  
-        int waterTrapped = 0;  
+        int water = 0;  
 
         while (left < right) {  
             if (height[left] < height[right]) {  
                 if (height[left] >= leftMax) {  
                     leftMax = height[left];  
                 } else {  
-                    waterTrapped += leftMax - height[left];  
+                    water += leftMax - height[left];  
                 }  
                 left++;  
             } else {  
                 if (height[right] >= rightMax) {  
                     rightMax = height[right];  
                 } else {  
-                    waterTrapped += rightMax - height[right];  
+                    water += rightMax - height[right];  
                 }  
                 right--;  
             }  
         }  
 
-        return waterTrapped;  
+        return water;  
     }  
 }
